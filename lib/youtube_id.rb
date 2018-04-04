76addca560b8528ff57b29f7e97009fcdc692635
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2018-04-04 16:03:55 +0300

    Add changes to format of short youtube links

--- lib/youtube_id.rb
+++ lib/youtube_id.rb
@@ -2,12 +2,12 @@ require "youtube_id/version"
 
 module YoutubeID
   FORMATS = [
-    %r((?:https?://)?youtu\.be/(.+)),
+    %r((?:https?://)?youtu\.be/(.*?)(\?|$|#|&)),
     %r((?:https?://)?(?:www\.)?youtube\.com/watch\?v=(.*?)(&|#|$)),
     %r((?:https?://)?(?:www\.)?youtube\.com/embed/(.*?)(\?|$)),
     %r((?:https?://)?(?:www\.)?youtube\.com/v/(.*?)(#|\?|$)),
     %r((?:https?://)?(?:www\.)?youtube\.com/user/.*?#\w/\w/\w/\w/(.+)\b)
-  ]
+  ].freeze
 
   def self.from(video_url)
     video_url.strip!
